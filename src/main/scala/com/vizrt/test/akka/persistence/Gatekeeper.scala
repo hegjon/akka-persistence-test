4777d553f1847ed984b7cfc3c12c63d4ccc19323
Jonny Heggheim <hegjon@gmail.com>
2014-10-28 11:45:48 +0100

    Removed case classes for Transfer and NewTransfer

--- src/main/scala/com/vizrt/test/akka/persistence/Gatekeeper.scala
+++ src/main/scala/com/vizrt/test/akka/persistence/Gatekeeper.scala
@@ -2,22 +2,23 @@ package com.vizrt.test.akka.persistence
 
 import akka.actor.{ActorLogging, ActorRef, Props}
 import akka.persistence.PersistentActor
+import com.vizrt.test.akka.persistence.Messages2._
 
 class Gatekeeper extends PersistentActor with ActorLogging {
-  private var activeTransfers = Map[Messages2.Transfer, ActorRef]()
+  private var activeTransfers = Map[Transfer, ActorRef]()
 
   override def persistenceId: String = "transfers"
 
   override def receiveRecover = {
-    case t: Messages2.NewTransfer => newTransfer2(t)
+    case t: NewTransfer => newTransfer2(t)
   }
 
   override def receiveCommand = {
-    case t: Messages2.NewTransfer => persist(t)(newTransfer2)
+    case t: NewTransfer => persist(t)(newTransfer2)
   }
 
-  private def newTransfer2(t: Messages2.NewTransfer): Unit = {
-    log.info(s"NewTransfer2: $t")
+  private def newTransfer2(t: NewTransfer): Unit = {
+    log.info(t.toString)
     val transfer = t.getTransfer
     val transferActor = context.actorOf(Props(classOf[TransferActor], transfer), transfer.getId)
     activeTransfers += (transfer -> transferActor)
--- src/main/scala/com/vizrt/test/akka/persistence/Messages.scala
+++ src/main/scala/com/vizrt/test/akka/persistence/Messages.scala
@@ -2,11 +2,6 @@ package com.vizrt.test.akka.persistence
 
 import java.net.URI
 
-//mosXml is a gigant XML that is the input we get from end user / external system. Simplified in this test project
-case class Transfer(id: String, created: Long, mosXml: String)
-
-case class NewTransfer(transfer: Transfer)
-
 case class StartExport(transferMedia: URI)
 case object ExportSuccess
 
